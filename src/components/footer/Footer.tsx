4eb174e769f7ff01f8b7d7c3e4a843dacb18860e
Nipun Chamika <nipunchamika11@gmail.com>
2023-12-18 18:37:42 +0530

    Build a footer

--- src/components/footer/Footer.tsx
+++ src/components/footer/Footer.tsx
@@ -1,5 +1,12 @@
+import "./footer.css";
+
 const Footer = () => {
-  return <div>Footer</div>;
+  return (
+    <div className="footer flex align-items-center justify-content-between p-4">
+      <span className="font-semibold">Swiftly</span>
+      <span className="text-sm">© Nipun Chamika Admin Panel</span>
+    </div>
+  );
 };
 
 export default Footer;
