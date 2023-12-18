bda8b2486a7226e3311047d362516fbff5228f42
Nipun Chamika <nipunchamika11@gmail.com>
2023-12-18 12:45:35 +0530

    Build a layout

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,54 @@
+import { Outlet, RouterProvider, createBrowserRouter } from "react-router-dom";
+import Home from "./pages/home/Home";
+import Users from "./pages/users/Users";
+import NavBar from "./components/navBar/NavBar";
+import Footer from "./components/footer/Footer";
+import Menu from "./components/menu/Menu";
+import Login from "./pages/login/Login";
+import "./styles/global.css";
+
 function App() {
+  const Layout = () => {
+    return (
+      <div className="main">
+        <NavBar />
+        <div className="container flex">
+          <div className="menuContainer w-15rem py-2 px-3">
+            <Menu />
+          </div>
+          <div className="contentContainer">
+            <Outlet />
+          </div>
+        </div>
+        <Footer />
+      </div>
+    );
+  };
+
+  const router = createBrowserRouter([
+    {
+      path: "/",
+      element: <Layout />,
+      children: [
+        {
+          path: "/",
+          element: <Home />,
+        },
+        {
+          path: "/users",
+          element: <Users />,
+        },
+      ],
+    },
+    {
+      path: "/login",
+      element: <Login />,
+    },
+  ]);
+
   return (
     <>
-      <div></div>
+      <RouterProvider router={router} />
     </>
   );
 }
--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,15 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.tsx'
-import './index.css'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App.tsx";
 
-ReactDOM.createRoot(document.getElementById('root')!).render(
+// PrimeReact CSS
+import "primereact/resources/themes/saga-blue/theme.css"; // theme
+import "primereact/resources/primereact.min.css"; // core css
+import "primeicons/primeicons.css"; // icons
+import "primeflex/primeflex.css"; // flex
+
+ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
     <App />
-  </React.StrictMode>,
-)
+  </React.StrictMode>
+);
